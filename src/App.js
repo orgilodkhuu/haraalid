3e299e88a4ef357b3ae52c7de38fd46b3cb4eca0
abi to <onsummit7@gmail.com>
2021-03-25 19:03:06 +0800

    t

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import './App.css';
 import './styles/flex.scss';
+import pepeImage from './pepe.jpeg';
+import navLeftSvg from './mf-logo.svg';
+import pc from './hero.svg';
 
 function App() {
   return (
-    <div className="cont">
-      <div className="nav">
-        <div id="c">
-          <div className="point">About</div>
-          <div className="point">Skills</div>
-          <div className="point">Projects</div>
+    <div className="cont flex-col">
+      <section className="content">
+        <div className="nav flex-row justify-between">
+          <img className="nav-left" src={navLeftSvg} />
+          <div className="nav-right flex-row">
+            <div className="point">About</div>
+            <div className="point">Skills</div>
+            <div className="point">Projects</div>
+          </div>
         </div>
-      </div>
-      <div id="content" className="flex-col items-center">
-        <div className="selfinfo">asdjknflaskjdflksajflk;as;dflsajfl;sadjfkasklsadkjl;saj;fjfdhfha;sldksadkfalsdfja;lsddlfsf;klsdkfj;lasdfkajsdfkl;j;alsjfl;asjfl;asjfkalsdjfl;sadfjkl;sdfj;lasdkjfdsl;fjkaldsjfa;lsdflsdflsfksdf</div>
-      </div>
+      </section>
+      <section className="content about-section flex-col items-center">
+        <h1>
+          Designer, Front-end Developer & Mentor
+        </h1>
+        <h2>
+          I design and code beautifully simple things, and I love what I do.
+        </h2>
+        <img src={pepeImage} style={{ borderRadius: "100%", width: '200px', height: '200px', marginTop: '2%', marginBottom: '4%' }} />
+        <img src={pc} />
+      </section>
+      <section className="about">
+
+      </section>
     </div>
   );
 }
